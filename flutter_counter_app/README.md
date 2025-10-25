4a702f8a84b08deed13871bcb5ef363502d83561
Ahsan Habib <shehanuk.ahsan@gmail.com>
2025-10-25 16:44:37 +0600

    feat: cubit counter

---
+++ flutter_counter_app/README.md
@@ -0,0 +1,6 @@
+# flutter_counter_app
+
+A new Flutter project.
+
+
+https://www.youtube.com/watch?v=SDk_GldOtK8
---
+++ flutter_counter_app/lib/increment_decrement_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class IncrementDecrementPage extends StatelessWidget {
+  const IncrementDecrementPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterIncremented());
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              // counterCubit.decrement();
+              counterBloc.add(CounterDecremented());
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
