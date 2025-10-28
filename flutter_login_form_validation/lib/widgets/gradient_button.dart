2d445b6fc414893062abc6e863b2a6e8c8107b25
Ahsan Habib <shehanuk.ahsan@gmail.com>
2025-10-28 14:52:19 +0600

    starter files added

---
+++ flutter_login_form_validation/lib/widgets/gradient_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../palette.dart';
+
+class GradientButton extends StatelessWidget {
+  const GradientButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Palette.gradient1, Palette.gradient2, Palette.gradient3],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(395, 55),
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+        ),
+        child: const Text(
+          'Sign in',
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_login_form_validation/lib/widgets/social_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../palette.dart';
+
+class SocialButton extends StatelessWidget {
+  final String iconPath;
+  final String label;
+  final double horizontalPadding;
+  const SocialButton({
+    Key? key,
+    required this.iconPath,
+    required this.label,
+    this.horizontalPadding = 100,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: () {},
+      icon: SvgPicture.asset(iconPath, width: 25, color: Palette.whiteColor),
+      label: Text(
+        label,
+        style: const TextStyle(color: Palette.whiteColor, fontSize: 17),
+      ),
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.symmetric(
+          vertical: 30,
+          horizontal: horizontalPadding,
+        ),
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(color: Palette.borderColor, width: 3),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
