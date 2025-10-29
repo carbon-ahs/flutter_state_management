986225c0eacd3966a8b7a258508005e56d1819ee
Ahsan Habib <shehanuk.ahsan@gmail.com>
2025-10-29 16:24:44 +0600

    chore: codebase cleaned

--- flutter_login_form_validation/lib/bloc/auth_bloc.dart
+++ flutter_login_form_validation/lib/bloc/auth_bloc.dart
@@ -6,32 +6,42 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        final email = event.email;
-        final password = event.password;
+    on<AuthLoginRequested>(_onAuthLoginRequested);
 
-        if (email.isEmpty || !email.contains('@')) {
-          return emit(AuthFailed('Please enter a valid email'));
-        }
-        if (password.length < 6) {
-          emit(AuthFailed('Password must be at least 6 characters'));
-          return;
-        }
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
+  }
 
-        await Future.delayed(const Duration(seconds: 3), () {
-          emit(AuthSuccess('$email-$password'));
-        });
-      } catch (e) {
-        emit(AuthFailed(e.toString()));
+  void _onAuthLoginRequested(
+    AuthLoginRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      final email = event.email;
+      final password = event.password;
+
+      if (email.isEmpty || !email.contains('@')) {
+        return emit(AuthFailed('Please enter a valid email'));
       }
-    });
+      if (password.length < 6) {
+        emit(AuthFailed('Password must be at least 6 characters'));
+        return;
+      }
 
-    on<AuthLogoutRequested>((event, emit) async {
-      emit(AuthLoading());
-      await Future.delayed(const Duration(seconds: 2), () {});
-      emit(AuthInitial());
-    });
+      await Future.delayed(const Duration(seconds: 3), () {
+        emit(AuthSuccess('$email-$password'));
+      });
+    } catch (e) {
+      emit(AuthFailed(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(
+    AuthLogoutRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    await Future.delayed(const Duration(seconds: 2), () {});
+    emit(AuthInitial());
   }
 }
