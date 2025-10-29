85c0389ef6b8aa0ba341be1f44f695b0fdb3e15d
Ahsan Habib <shehanuk.ahsan@gmail.com>
2025-10-29 16:58:44 +0600

    push from office

--- flutter_weather_app/README.md
+++ flutter_weather_app/README.md
@@ -1,3 +1,6 @@
 # flutter_weather_app
 
 A new Flutter project.
+
+
+https://youtu.be/SDk_GldOtK8?si=Yoh_p_uc54BSr_C5&t=7295
--- flutter_weather_app/lib/main.dart
+++ flutter_weather_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'weather_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -12,7 +14,8 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true),
-      home: const Scaffold(body: Center(child: Text('Hello World!'))),
+      // home: const Scaffold(body: Center(child: Text('Hello World!'))),
+      home: const WeatherScreen(),
     );
   }
 }
